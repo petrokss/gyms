602ea3e6d2c1e84b7554bc42be962f6cf3d03842
Oksana <petrokss9@gmail.com>
2019-01-29 12:42:09 +0200

    initialize proj

---
+++ app.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let i = 0;
+
+// function handleRequest(req, res) {
+//   console.log("handleRequest", ++i);
+//   responseText = new Date().toTimeString();
+//   res.send(responseText);
+// }
+
+// function handleRequest2(req, res) {
+//   console.log("handleRequest2", ++i);
+//   res.send(Math.random() * 1000 + "");
+// }
+
+// ``
+// app.post("/abc", handleRequest2);
+
+// app.listen(3000);
+// app.listen(3000);
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+// app.post('/', function (req, res) {
+//   res.send('Got a POST request');
+// });
+// app.listen(3000);
