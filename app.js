09b60b45ab4413a2c44b284c426fbe2200b7fefb
Oksana <petrokss9@gmail.com>
2019-02-28 11:13:35 +0200

    res.render

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 let express = require("express");
 let path = require('path');
 let app = express();
-var exphbs  = require('express-handlebars');
+let exphbs  = require('express-handlebars');
 let bodyParser = require('body-parser');
 const Database = require('better-sqlite3');
 const db = new Database('dataBase.db', { verbose: console.log });
 const stmt = db.prepare('INSERT INTO users (login, password) VALUES (?, ?)');
 
-let hbs = exphbs.create({defaultLayout: 'home'});
+let hbs = exphbs.create();
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  //res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.render('home');
 });
 
 app.use(bodyParser.json());
