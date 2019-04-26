1a71597bc344258adfdd71f3d0987994dbd126f5
Oksana <petrokss9@gmail.com>
2019-04-26 17:38:05 +0300

    create gym

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ let exphbs  = require('express-handlebars');
 let bodyParser = require('body-parser');
 const Database = require('better-sqlite3');
 const db = new Database('gyms.db', { verbose: console.log });
-//const stmt = db.prepare('INSERT INTO users (login, password) VALUES (?, ?)');
+const stmt = db.prepare('INSERT INTO gyms (name, adress, photo) VALUES (?, ?, ?)');
 const row = db.prepare('SELECT * FROM gyms');
 const rowId = db.prepare('SELECT * FROM gyms WHERE id = ?');
+// const createGym = db.prepare('INSERT INTO gyms (name, adress, photo) VALUES (?, ?, ?)');
 
 let hbs = exphbs.create();
 
@@ -17,6 +18,10 @@ app.set('view engine', 'handlebars');
 app.get('/', function(req, res) {
   app.use(express.static('public'));
   res.render('home', {data: row.all()});
+});
+
+app.get('/gyms/create', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/creategym.html'));
 });
 
 app.get('/gyms/:id', function(req, res) {
@@ -32,6 +37,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/', function(req, res) {
   //console.log(req.body);
   //stmt.run(req.body.name, req.body.password)
+  res.send("recieved your request!");
+});
+
+app.post('/gyms/create', function(req, res) {
+  console.log(req.body);
+  stmt.run(req.body.name, req.body.adress, req.body.photo);
   res.send("recieved your request!");
 });
 
